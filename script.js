2f62d89e01b291365815798db6efc52afac88a7b
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-30 19:22:50 +0500

    init

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let canvasSize = 270,
+var canvasSize = 270,
     centre = canvasSize/2,
     radius = canvasSize*0.8/2,
     s = Snap('#svg'),
@@ -15,4 +15,28 @@ input.onkeyup = function(evt) {
     }
 };
 
+function run(percent) {
+    var endpoint = percent * 360;
+    Snap.animate(0, endpoint, function (val) {
+        arc.remove();
 
+        var d = val,
+            dr = d - 90;
+        radians = Math.PI * (dr) / 180,
+            endx = centre + radius * Math.cos(radians),
+            endy = centre + radius * Math.sin(radians),
+            largeArc = d > 180 ? 1 : 0;
+        path = "M" + centre + "," + startY + " A" + radius + "," + radius + " 0 " + largeArc + ",1 " + endx + "," + endy;
+
+        arc = s.path(path);
+        arc.attr({
+            stroke: '#ffcec9',
+            fill: 'none',
+            strokeWidth: 35
+        });
+        percDiv.innerHTML = Math.round(val / 360 * 100) + '%';
+
+    }, 2000, mina.easeinout);
+}
+
+run(input.value / 100);
