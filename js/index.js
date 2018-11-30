9f66b549a9c5eda2c4bb9f4b6942195284c339be
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-30 19:28:27 +0500

    init

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 var canvasSize = 270,
-    centre = canvasSize/2,
-    radius = canvasSize*0.8/2,
+    centre = canvasSize / 2,
+    radius = canvasSize * 0.8 / 2,
     s = Snap('#svg'),
     path = "",
     arc = s.path(path),
-    startY = center-radius,
+    startY = centre - radius,
     runBtn = document.getElementById('run'),
     percDiv = document.getElementById('percent'),
     input = document.getElementById('input');
 
-input.onkeyup = function(evt) {
-    if(isNaN(input.value)) {
+input.onkeyup = function (evt) {
+    if (isNaN(input.value)) {
         input.value = '';
     }
+};
+
+runBtn.onclick = function () {
+    run(input.value / 100);
 };
 
 function run(percent) {
